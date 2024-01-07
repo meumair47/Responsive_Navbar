383d0aebf281859fa1da081c81a4df8826347d07
Muhammad Umair <meumairkhan47@gmail.com>
2024-01-08 00:44:51 +0500

    Responsive navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./components/Nav";
+
+const App = () => {
+
+  return (
+    <>
+      <div className="bg-slate-900">
+        <Nav/>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
